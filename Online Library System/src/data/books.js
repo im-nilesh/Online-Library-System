4506042f6960e66fc0ec1d523cadb48c319144ee
im-nilesh <nileshdadab@gmail.com>
2026-06-03 14:59:00 +0530

    added dummy data of books

---
+++ Online Library System/src/data/books.js
@@ -0,0 +1,34 @@
+export const books = [
+  {
+    id: 1,
+    title: "Harry Potter",
+    author: "J.K Rowling",
+    category: "Fiction",
+    rating: 4.8,
+    description: "A magical fantasy novel.",
+  },
+  {
+    id: 2,
+    title: "Dune",
+    author: "Frank Herbert",
+    category: "Sci-Fi",
+    rating: 4.7,
+    description: "Science fiction masterpiece.",
+  },
+  {
+    id: 3,
+    title: "Atomic Habits",
+    author: "James Clear",
+    category: "Non-Fiction",
+    rating: 4.9,
+    description: "Book about building habits.",
+  },
+  {
+    id: 4,
+    title: "Dracula",
+    author: "Bram Stoker",
+    category: "Horror",
+    rating: 4.6,
+    description: "Classic vampire novel.",
+  },
+];
